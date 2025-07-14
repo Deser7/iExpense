92c2e5da62bd2e75d75e21281ec9d9efffcfba11
Deser7 <deser7@icloud.com>
2025-07-14 17:06:01 +0400

    создал новый проект

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Наташа Спиридонова on 14.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
