a606dc63072138436946cf2622ee16a313480b2c
Deser7 <deser7@icloud.com>
2025-07-15 16:46:30 +0400

    добавил кнопку save

--- iExpense/Expenses.swift
+++ iExpense/Expenses.swift
@@ -6,8 +6,25 @@
 //
 
 import Observation
+import Foundation
 
 @Observable
 final class Expenses {
-    var items: [ExpenseItem] = []
+    var items: [ExpenseItem] = [] {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decoderItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decoderItems
+                return
+            }
+        }
+        items = []
+    }
 }
