17d0e2fc90f7366ca1b738ed3fedf940b22497cd
Deser7 <deser7@icloud.com>
2025-07-15 16:27:04 +0400

    сделал переход между экранами

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Наташа Спиридонова on 14.07.2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
