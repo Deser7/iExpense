fde90ffeeb3156daedc05f638b1def3400f137a2
Deser7 <deser7@icloud.com>
2025-07-17 15:26:35 +0400

    разделил экраны расходов

---
+++ iExpense/ExpensesListView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpensesListView.swift
+//  iExpense
+//
+//  Created by Наташа Спиридонова on 16.07.2025.
+//
+
+import SwiftUI
+
+struct ExpensesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpensesListView()
+}
